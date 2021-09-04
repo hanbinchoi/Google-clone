ece45c7cd2b4a25205c4f51e735eed5578085150
hanbinchoi <wkghskak@naver.com>
2021-09-04 14:31:51 +0900

    ..

---
+++ main.js
@@ -0,0 +1,10 @@
+const moreBtn = document.querySelector(".info .metadata .titleAndButton .moreBtn");
+const title = document.querySelector(".info .metadata .titleAndButton .title");
+
+console.log(moreBtn);
+
+moreBtn.addEventListener('click', () => {
+    moreBtn.classList.toggle('clicked');
+    title.classList.toggle('clamp');
+    console.log("asd");
+});
